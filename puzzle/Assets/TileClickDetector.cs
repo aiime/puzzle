eaaafdaba3aae85b9f8b96a5f61a810af732eae7
aiime <gtfxtiro@protonmail.com>
2018-10-14 22:43:10 +0500

    Add test 3x3 image and basic control.

---
+++ puzzle/Assets/TileClickDetector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Puzzle.Game
+{
+    public class TileClickDetector : MonoBehaviour, IPointerClickHandler
+    {
+        public TileController tileController;
+        public int tileNo;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            tileController.OnTileClicked(this);
+        }
+    }
+}
---
+++ puzzle/Assets/TileController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Puzzle.Game
+{
+    public class TileController : MonoBehaviour
+    {
+        public Image[] imagePiecesHolders;
+        public Sprite[] imagePieces;
+        
+        private Transform firstTileTransform = null;
+        private Transform secondTileTransform = null;
+
+        private void Awake()
+        {
+            for (int i = 0; i < imagePieces.Length; i++)
+            {
+                imagePiecesHolders[i].sprite = imagePieces[i];
+            }
+        }
+
+        public void OnTileClicked(TileClickDetector tileClicked)
+        {
+            if (firstTileTransform == null)
+            {
+                firstTileTransform = tileClicked.transform;
+            }
+            else
+            {
+                secondTileTransform = tileClicked.transform;
+                SwapTiles();
+            }
+        }
+
+        private void SwapTiles()
+        {
+            Vector2 temp = firstTileTransform.position;
+            firstTileTransform.position = secondTileTransform.position;
+            secondTileTransform.position = temp;
+            CleanReferences();
+        }
+
+        private void CleanReferences()
+        {
+            firstTileTransform = null;
+            secondTileTransform = null;
+        }
+    }
+}

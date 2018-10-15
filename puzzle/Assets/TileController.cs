538eabcb296224c933360e58cab633a11f41fdb2
aiime <gtfxtiro@protonmail.com>
2018-10-15 20:49:18 +0500

    Add image pieces shuffle.

--- puzzle/Assets/TileController.cs
+++ puzzle/Assets/TileController.cs
@@ -7,15 +7,17 @@ namespace Puzzle.Game
     {
         public Image[] imagePiecesHolders;
         public Sprite[] imagePieces;
-        
+
         private Transform firstTileTransform = null;
         private Transform secondTileTransform = null;
 
         private void Awake()
         {
+            int[] shuffleMask = CreateShuffleMask();
+
             for (int i = 0; i < imagePieces.Length; i++)
             {
-                imagePiecesHolders[i].sprite = imagePieces[i];
+                imagePiecesHolders[i].sprite = imagePieces[shuffleMask[i]];
             }
         }
 
@@ -44,6 +46,22 @@ namespace Puzzle.Game
         {
             firstTileTransform = null;
             secondTileTransform = null;
+        }
+
+        private int[] CreateShuffleMask()
+        {
+            int[] shuffleMask = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // Тасование Фишера-Йетса
+            for (int i = shuffleMask.Length; i > 0; i--)
+            {
+                int randomCellToSwap = Random.Range(0, i);
+                int tmp = shuffleMask[randomCellToSwap];
+                shuffleMask[randomCellToSwap] = shuffleMask[i - 1];
+                shuffleMask[i - 1] = tmp;
+            }
+
+            return shuffleMask;
         }
     }
 }

e5349dde84a2fe3e94cb7793a1a080b458ec77df
aiime <gtfxtiro@protonmail.com>
2018-10-29 23:38:49 +0500

    Add options panel with open/close behaviour.

---
+++ puzzle/Assets/ConfirmButtonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmButtonController : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject optionsPanelBlocker;
+    [SerializeField] private GameObject backBlocker;
+    [SerializeField] private CanvasGroup optionsPanelCanvasGroup;
+    [SerializeField] private CanvasGroup backBlockerCanvasGroup;
+    [SerializeField] private float timeToFade;
+
+    private float currentTimeForOptionPanel;
+    private float currentTimeForBackBlockerPanel;
+
+    public void OnConfirmClick()
+    {
+        StartCoroutine(FadeOutOptionsPanel());
+        StartCoroutine(FadeOutBackBlockerPanel());
+    }
+
+    private IEnumerator FadeOutOptionsPanel()
+    {
+        optionsPanelBlocker.SetActive(true);
+
+        while (optionsPanelCanvasGroup.alpha != 0)
+        {
+            currentTimeForOptionPanel += Time.deltaTime;
+            optionsPanelCanvasGroup.alpha = Mathf.Lerp(1, 0, currentTimeForOptionPanel / timeToFade);
+            yield return null;
+        }
+
+        optionsPanel.SetActive(false);
+        optionsPanelBlocker.SetActive(false);
+        backBlocker.SetActive(false);
+        currentTimeForOptionPanel = 0;
+    }
+
+    private IEnumerator FadeOutBackBlockerPanel()
+    {
+        while (backBlockerCanvasGroup.alpha != 0)
+        {
+            currentTimeForBackBlockerPanel += Time.deltaTime;
+            backBlockerCanvasGroup.alpha = Mathf.Lerp(1, 0, currentTimeForBackBlockerPanel / timeToFade);
+            yield return null;
+        }
+
+        currentTimeForBackBlockerPanel = 0;
+    }
+}
---
+++ puzzle/Assets/OptionsBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject optionsPanelBlocker;
+    [SerializeField] private GameObject backBlocker;
+    [SerializeField] private CanvasGroup optionsPanelCanvasGroup;
+    [SerializeField] private CanvasGroup backBlockerCanvasGroup;
+    [SerializeField] private float timeToFade;
+
+    private float currentTimeForOptionPanel;
+    private float currentTimeForBackBlockerPanel;
+    
+    public void OnOptionsClick()
+    {
+        StartCoroutine(FadeInOptionsPanel());
+        StartCoroutine(FadeInBackBlockerPanel());
+    }
+
+    private IEnumerator FadeInOptionsPanel()
+    {
+        backBlocker.SetActive(true);    
+        optionsPanel.SetActive(true);
+        optionsPanelBlocker.SetActive(true);
+
+        while (optionsPanelCanvasGroup.alpha != 1)
+        {
+            currentTimeForOptionPanel += Time.deltaTime;
+            optionsPanelCanvasGroup.alpha = Mathf.Lerp(0, 1, currentTimeForOptionPanel / timeToFade);
+            yield return null;
+        }
+
+        optionsPanelBlocker.SetActive(false);
+        currentTimeForOptionPanel = 0;
+    }
+
+    private IEnumerator FadeInBackBlockerPanel()
+    {
+        while (backBlockerCanvasGroup.alpha != 1)
+        {
+            currentTimeForBackBlockerPanel += Time.deltaTime;
+            backBlockerCanvasGroup.alpha = Mathf.Lerp(0, 1, currentTimeForBackBlockerPanel / timeToFade);
+            yield return null;
+        }
+
+        currentTimeForBackBlockerPanel = 0;
+    }
+}

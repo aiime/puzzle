543ea8c74bea8558f55533c34c28ae7c1eaf0831
aiime <gtfxtiro@protonmail.com>
2018-11-05 00:34:31 +0500

    Add audio system.

---
+++ puzzle/Assets/Scripts/Puzzle/Audio/EffectsVolumeSliderController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Puzzle.Audio
+{
+    [AddComponentMenu("Puzzle/Audio/Effects Volume Slider Controller")]
+    public class EffectsVolumeSliderController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private Slider slider;
+
+        public void OnSliderValueChanged()
+        {
+            audioSource.volume = slider.value;
+        }
+    }
+}
+
---
+++ puzzle/Assets/Scripts/Puzzle/Audio/MusicVolumeSliderController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Puzzle.Audio
+{
+    [AddComponentMenu("Puzzle/Audio/Music Volume Slider Controller")]
+    public class MusicVolumeSliderController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private Slider slider;
+
+        public void OnSliderValueChanged()
+        {
+            audioSource.volume = slider.value;
+        }
+    }
+}
